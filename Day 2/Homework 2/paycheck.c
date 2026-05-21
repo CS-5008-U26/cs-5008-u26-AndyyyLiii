3626eb186a0acf004ab51bc4dcafaa3b443ae089
Wenhao Li <li.wenha@northeastern.edu>
2026-05-20 20:56:50 -0700

    Homework2-1

---
+++ Day 2/Homework 2/paycheck.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main () {
+    double rate,hours;
+    while (1) {
+    printf("Enter the hourly rate: ");
+    scanf("%lf", &rate);
+
+    if (rate <= 0) {
+        break;
+    }
+
+    printf("Enter the number of hours worked: ");
+    scanf("%lf", &hours);
+
+    if (hours <= 0) {
+        break;
+    }
+
+    if (hours<=40) {
+        printf("Paycheck: $%.2lf\n", rate*hours);
+    } else {
+        printf("Paycheck: $%.2lf\n", (rate*40)+(rate*1.5*(hours-40)));
+    }    /* code */
+    }
+    
+
+    
+
+   
+  
+    
+    return 0;   
+}
